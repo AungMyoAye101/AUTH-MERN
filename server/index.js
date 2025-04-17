50847bba463c1b89a9c1ebd24ec7931c8342c5cd
Aung Myo Aye <aungmyoaye101@gmail.com>
2025-04-17 21:58:43 +0630

    FEAT add cors  and cookie-parser middlewares

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
 import cors from "cors"
+import cookieParser from "cookie-parser"
 
 const app = express()
 
@@ -8,7 +9,11 @@ dotenv.config()
 const port = process.env.PORT || 4000
 
 app.use(express.json())
-app.use(cors, { credients: true })
+app.use(cookieParser())
+app.use(cors({ credentials: true }))
 
+
+
+app.get('/', (req, res) => res.send("Server is alive."))
 app.listen(port, () => console.log("Server is listening on port " + port))
 
