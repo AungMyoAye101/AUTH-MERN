36148598e550b067c6b0cc7981e93024fbf94e90
Aung Myo Aye <aungmyoaye101@gmail.com>
2025-04-18 11:58:06 +0630

    FEAT: improve user creation logic, and enhance login validation

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -12,12 +12,12 @@ export const register = async (req, res) => {
     try {
         const existingUser = await User.findOne({ email })
         if (existingUser) {
-            return res.status(400).json({ success: false, message: "User already exist!" })
+            return res.status(400).json({ success: false, message: "User already exists!" })
         }
 
         const hashedPassword = await bcrypt.hash(password, 10)
 
-        const user = await User.create({ name, email, password: hashedPassword })
+        const user = new User({ name, email, password: hashedPassword })
 
         const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "1d" })
 
@@ -27,6 +27,7 @@ export const register = async (req, res) => {
             maxAge: 1 * 60 * 60 * 1000
 
         })
+        await user.save()
 
         return res.status(200).json({ success: true, message: "success" })
 
@@ -36,9 +37,33 @@ export const register = async (req, res) => {
 
 }
 export const login = async (req, res) => {
+    const { email, password } = req.body
+    if (!email || !password) {
+        return res.status(400).json({ success: false, message: "All fields are required!" })
+    }
     try {
+        const user = await User.findOne({ email })
+        if (!user) {
+            return res.status(404).json({ success: false, message: "User not found!" })
+        }
 
-        return res.status(200).json({ success: true, message: "success" })
+        const isMatch = await bcrypt.compare(password, user.password)
+
+        if (!isMatch) {
+            return res.status(400).json({ success: false, message: "Password not correct!" })
+        }
+
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+            expiresIn: "1d"
+        })
+
+        res.cookie("token", token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            maxAge: 1 * 60 * 60 * 1000
+        })
+
+        return res.status(200).json({ success: true, message: "success", user })
 
     } catch (error) {
         return res.status(500).json({ success: false, message: error.message })
@@ -54,4 +79,4 @@ export const logout = async (req, res) => {
         return res.status(500).json({ success: false, message: error.message })
     }
 
-}
+} 
