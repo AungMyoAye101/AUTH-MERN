a9b91698622809f5d36b919265d3ac05bb0a0c85
Aung Myo Aye <aungmyoaye101@gmail.com>
2025-04-18 08:14:54 +0630

    FEAT: implement user registration with validation and JWT token generation

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,5 +1,30 @@
+import jwt from "jsonwebtoken"
+import User from "../models/User.model.js"
+
+
 export const register = async (req, res) => {
+    const { name, email, password } = req.body
+    if (!name || !email || password) {
+        return res.status(400).json({ success: false, message: "All fileds are required!" })
+    }
     try {
+        const existingUser = await User.findOne({ email })
+        if (existingUser) {
+            return res.status(400).json({ success: false, message: "User already exist!" })
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10)
+
+        const user = await User.create({ name, email, password: hashedPassword })
+
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "1d" })
+
+        res.cookie("token", token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            maxAge: 1 * 60 * 60 * 1000
+
+        })
 
         return res.status(200).json({ success: true, message: "success" })
 
