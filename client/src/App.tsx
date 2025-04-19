e5d98cc7fdd96ba710a9b6434079655322801c00
Aung Myo Aye <aungmyoaye101@gmail.com>
2025-04-19 22:58:29 +0630

    FEAT:add frontend folders and file that were required.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,28 @@
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+import OtpVerify from './pages/OtpVerify'
+import Dashboard from './pages/Dashboard'
+import Profile from './pages/Profile'
 
 function App() {
 
 
   return (
-    <>
-      <h1>hello react</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/verify' element={<OtpVerify />} />
+        <Route path='/user/:id' element={<Profile />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+
   )
 }
 
---
+++ client/src/components/ui/Button.tsx
@@ -0,0 +1,9 @@
+
+
+const Button = () => {
+    return (
+        <button>submit</button>
+    )
+}
+
+export default Button
