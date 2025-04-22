898551204888bbb491db3ffca27bca0a7e446652
Aung Myo Aye <aungmyoaye101@gmail.com>
2025-04-22 11:59:04 +0630

    feat: update AccountVerify to integrate OTP timer and display

--- client/src/components/ui/FormController.tsx
+++ client/src/components/ui/FormController.tsx
@@ -1,4 +1,6 @@
 type InputType = {
+    data?: any,
+    disable?: boolean
     type: string,
     name: string,
     id: string,
@@ -8,10 +10,10 @@ type InputType = {
 
 }
 
-const FormController = ({ type = 'text', name, id, placeholder, onChange, style }: InputType) => {
+const FormController = ({ data, disable, type = 'text', name, id, placeholder, onChange, style }: InputType) => {
     return (
         <label htmlFor={id}>
-            <input type={type} name={name} id={id} onChange={onChange} placeholder={placeholder} className={`h-10 px-4 w-full border focus:outline-none bg-gray-100 rounded-lg ${style}`} />
+            <input value={data} disabled={disable ? true : false} type={type} name={name} id={id} onChange={onChange} placeholder={placeholder} className={`h-10 px-4 w-full border focus:outline-none bg-gray-100 rounded-lg ${style}`} />
         </label>
     )
 }
--- client/src/pages/AccountVerify.tsx
+++ client/src/pages/AccountVerify.tsx
@@ -4,9 +4,14 @@ import FormController from '../components/ui/FormController'
 import { base_url } from './Signup'
 import { showToast } from '../context/ToastProvider'
 
+
+const OTP_EXPRIES_IN = 5 * 60 // 5 minutes
+
 const AccountVerify = () => {
     const [data, setData] = useState({ otp: '' })
     const [error, setError] = useState()
+
+    const [otpTimer, setOtpTimer] = useState(OTP_EXPRIES_IN)
     const hasSentOTP = useRef(false)
     console.log(data, " in OTP ")
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -14,6 +19,7 @@ const AccountVerify = () => {
         setData((pre) => ({ ...pre, [name]: value }))
     }
 
+    // request OTP from server
     const otpSender = async () => {
         try {
             const res = await fetch(base_url + 'auth/verify', {
@@ -45,16 +51,39 @@ const AccountVerify = () => {
 
     }, [])
 
+    //OTP expire count down
+    useEffect(() => {
+        if (otpTimer === 0) {
+            showToast('warn', "Your OTP code has expired , Request new one.")
+            return
+        }
 
 
+        const intervelId = setInterval(() => {
+            setOtpTimer(pre => pre - 1)
+        }, 1000)
+        return () => clearInterval(intervelId)
+    }, [otpTimer])
+
+    // formatting for mm:ss
+    const formatTime = (seconds: number) => {
+        const min = Math.floor(seconds / 60)
+
+        const sec = (seconds % 60).toString();
+        return `${min}:${sec}`;
+    };
     return (
-        <section>
+        <section className='flex justify-center'>
             <Form headingText='Account Verification' endpoint='auth/verify_account' method='POST' data={data} setError={setError} redirect={'/'}>
                 <h2 className=' font-serif text-center'>We have send OTP code to your email.</h2>
-                <FormController type='text' name='otp' id='otp' onChange={handleChange} placeholder='Please enter OTP code' />
+                <div className='text-sm font-medium text-neutral-600'> OTP expires in <span className='font-medium'>{formatTime(otpTimer)}</span></div>
+                <FormController data={data.otp} type='number' name='otp' id='otp' onChange={handleChange} placeholder='Please enter OTP code' />
+
                 {
                     error && <div className='text-red-400 text-sm'>{error}</div>
                 }
+
+
 
             </Form>
         </section>
