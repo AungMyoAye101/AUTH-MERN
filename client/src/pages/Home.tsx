7e2c6f81a98b39d5b64c57981d0d9687fc4c9e4d
Aung Myo Aye <aungmyoaye101@gmail.com>
2025-04-24 20:03:44 +0630

    feat: update Home and Login components; conditionally render Dashboard link and fix endpoint path

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -10,8 +10,10 @@ const Home = () => {
             <div className="flex flex-col gap-4 items-start">
                 <h1 className="text-2xl md:4xl font-semibold font-serif ">Welcome {name ? name : "to Simple Auth"},</h1>
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos natus voluptatem aspernatur deserunt possimus, tenetur quis atque accusantium quod cumque esse eius reiciendis at incidunt magnam dicta iusto. Quae, iste. {email}</p>
-                <Link to={'/signup'} className="link_btn">Register now</Link>
 
+                {
+                    name ? <Link to={'/dashboard'} className="link_btn">Dashboard</Link> : <Link to={'/signup'} className="link_btn">Register now</Link>
+                }
             </div>
 
         </div>
