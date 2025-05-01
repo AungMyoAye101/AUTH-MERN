1b1d9be5f66465b4d1bc63db0496552902777874
Aung Myo Aye <aungmyoaye101@gmail.com>
2025-05-01 13:25:14 +0630

    Feat: enhance FormController layout and error display, update Login form error handling and submission logic

--- client/src/components/ui/FormController.tsx
+++ client/src/components/ui/FormController.tsx
@@ -25,24 +25,27 @@ const FormController = ({ type = 'text', name, placeholder, className, icon, reg
 
 
     return (
-        <label htmlFor={name} className={`h-10 w-full flex items-center  bg-neutral-200 rounded-lg text-sm overflow-hidden px-2 ${className}`}>
+        <label htmlFor={name} className={` overflow-hidden  ${className}`}>
+
+            <div className='h-10 w-full flex items-center  bg-neutral-200 rounded-lg text-sm px-2 '>
+
+                {
+                    icon && <img src={icon} alt="icon" className='w-5 ' />
+                }
+
+
+                <input type={inputType} id={name} {...(register ? register(name) : {})} placeholder={placeholder} className="w-full h-full border-none focus:outline-none bg-transparent ml-2 " />
+                {
+                    type === 'password' && (<>
+
+                        <img src={show ? "/assets/eye.svg" : "/assets/eye-slash.svg"} alt="eye icon" className='w-5 cursor-pointer' onClick={() => setshow(pre => !pre)} />
+
+
+                    </>)}
+            </div>
 
             {
-                icon && <img src={icon} alt="icon" className='w-5 ' />
-            }
-
-
-            <input type={inputType} id={name} {...(register ? register(name) : {})} placeholder={placeholder} className="w-full h-full border-none focus:outline-none bg-transparent ml-2 " />
-            {
-                type === 'password' && (<>
-
-                    <img src={show ? "/assets/eye.svg" : "/assets/eye-slash.svg"} alt="eye icon" className='w-5 cursor-pointer' onClick={() => setshow(pre => !pre)} />
-
-
-                </>)}
-
-            {
-                error && <p className='text-sm text-red-400'>{error.message}</p>
+                error && <p className='text-sm text-red-400 mt-1'>{error.message}</p>
             }
 
         </label>
--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -5,32 +5,30 @@ import FormController from '../components/ui/FormController'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { z } from 'zod'
+import { showToast } from '../context/ToastProvider'
 
 
 const loginSchema = z.object({
     email: z.string().email("Invalid email."),
-    password: z.string().min(6, "Password must be at least 6 characters long.").regex(/[A-Z]/, "Password must include one Uppercase letter").regex(/\d/, "Password must include one degit.")
+    password: z.string().min(6, "Password must be at least 6 characters long.").regex(/[A-Z]/, "Password must include one Uppercase letter").regex(/\d/, "Password must include one number.")
 })
 
 type LoginType = z.infer<typeof loginSchema>
 
 const Login = () => {
-    const [data, setData] = useState({
-        name: '',
-        email: "",
-        password: ''
-    })
+
 
     const [error, setError] = useState('')
 
     const { register, handleSubmit, formState: { errors } } = useForm({ resolver: zodResolver(loginSchema) })
 
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const { name, value } = e.target
-        setData((pre) => ({ ...pre, [name]: value }))
-    }
+    // const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    //     const { name, value } = e.target
+    //     setData((pre) => ({ ...pre, [name]: value }))
+    // }
 
     const onSubmit = async (data: LoginType) => {
+        console.log("click")
         try {
             const res = await fetch(base_url + "/auth/login", {
                 method: "POST",
@@ -42,7 +40,13 @@ const Login = () => {
             })
             const response = await res.json()
             console.log(response)
+            if (response.success === false) {
+                setError(response.message)
+                showToast("error", response.message)
+                return
+            }
         } catch (err: any) {
+            console.log(err)
             if (err.response.data.errors) {
                 setError(err.response.data.errors[0].msg)
             } else if (err.response.data.message) {
@@ -71,8 +75,14 @@ const Login = () => {
             <form onSubmit={handleSubmit(onSubmit)} className="min-w-lg w-96 max-w-xl bg-neutral-100 px-4 py-6 rounded-lg shadow-md border flex flex-col gap-3">
                 {
                     loginData.map(field => (
-                        <FormController name={field.name} icon={field.icon} register={register} placeholder={field.placeholder} type={field.type} error={errors?.email} />
+                        <FormController name={field.name} key={field.id} icon={field.icon} register={register} placeholder={field.placeholder} type={field.type} error={errors[field.name as keyof LoginType]} />
                     ))
+                }
+                <button type='submit'>Submit</button>
+                {
+
+                    error && <p className='text-sm bg-red-400 text-white p-2 rounded-full text-center'>{error}</p>
+
                 }
             </form>
         </section>
