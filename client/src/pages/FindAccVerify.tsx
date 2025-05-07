fd9c54ffbe4bcd8a4c3b872cf2903f278d7bb08c
Aung Myo Aye <aungmyoaye101@gmail.com>
2025-05-07 16:58:07 +0630

    FEAT: simplify FindAccVerify component by removing unused imports and OTP validation schema

--- client/src/pages/FindAccVerify.tsx
+++ client/src/pages/FindAccVerify.tsx
@@ -1,23 +1,5 @@
 
-import { useEffect, useState } from 'react'
-
-import FormController from '../components/ui/FormController'
-import { useNavigate, useSearchParams } from 'react-router-dom'
-import { showToast } from '../context/ToastProvider'
-import { useForm } from 'react-hook-form'
-import { z } from 'zod'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { base_url } from '../lib/helper'
-import Button from '../components/ui/Button'
 import OtpContainer from '../components/OtpContainer'
-
-
-
-const OTP_Schema = z.object({
-    otp: z.string().min(6, "OTP must be 6 characters").max(6, "OTP must be 6 characters"),
-    userId: z.string().optional()
-})
-
 
 const FindAccVerify = () => {
 
